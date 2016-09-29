8f512bce7d5f89324f2823fd64b7de16f97ba8b4
GustavoCardoso <gustavosantistaporpaixao12@gmail.com>
2016-09-29 14:57:49 -0300

    Add files via upload
    Script de IA simples com NaveMesh 3d

---
+++ Ponto.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ponto : MonoBehaviour {
+
+    public Transform atualTransforme;
+    public Transform[] coaminhos;
+    public NavMeshAgent navegador;
+
+    int i = 0;
+	void Start () {
+
+        navegador.destination = coaminhos[i].position;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float dist = Vector3.Distance(coaminhos[i].position, transform.position);
+        atualTransforme = coaminhos[i];
+
+        if (dist < 2)
+        {
+            if (i < coaminhos.Length - 1) //comparação usada para ver se o valor de i ja e maximo.
+            {
+                i++;
+                navegador.destination = coaminhos[i].position;
+
+            
+            }else
+            {
+                i--;
+                navegador.destination = coaminhos[i].position;
+            }
+        }
+
+    }
+
+
+
+}
