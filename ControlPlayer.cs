2e8f872f7fa66fee0c8e8830726ad61c5f50b829
GustavoCardoso <gustavosantistaporpaixao12@gmail.com>
2016-09-29 14:56:14 -0300

    Add files via upload
    Script simples de movimento 3d.

---
+++ ControlPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPlayer : MonoBehaviour {
+
+
+    private int velocity;
+
+    void Start ()
+    {
+        velocity = 5;
+
+	}
+	void Update ()
+    {
+       float x =  Input.GetAxis("Horizontal") * velocity * Time.deltaTime;
+       float z=  Input.GetAxis("Vertical") * velocity * Time.deltaTime;
+
+        transform.Translate(x,0,z);
+
+    }
+}
